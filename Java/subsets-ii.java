a98cdd080689bd5117fda70b0060e3a3de484873
jxq <jiangxq18@outlook.com>
2018-07-14 13:49:00 +0800

    Subsets II

--- Java/subsets-ii.java
+++ Java/subsets-ii.java
@@ -2,21 +2,21 @@
 
 // Time O(2^N), Space O(N)
 class Solution {
-    private List<List<Integer>> ans = new ArrayList<List<Integer>>();
-    private List<Integer> subset = new ArrayList<Integer>();
-    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        dfs(nums, 0);
-        return this.ans;
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> subset = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, 0, subset, ans);
+        return ans;
     }
     
-    private void dfs(int[] nums, int start) {
-        if (!this.ans.Contains(this.subset)) 
-            this.ans.add(this.subset);
+    private void dfs(int[] nums, int start, List<Integer> subset, List<List<Integer>> ans) {
+        if (ans.indexOf(subset) == -1)
+            ans.add(new ArrayList(subset));
         for (int i=start; i<nums.length; i++) {
-            this.subset.add(nums[i]);
-            dfs(nums, i+1);
-            this.subset.remove(this.subset.size()-1);
+            subset.add(nums[i]);
+            dfs(nums, i+1, subset, ans);
+            subset.remove(subset.size()-1);
         }
     }
 }
