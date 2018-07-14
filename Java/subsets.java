a03c9d366d6cb4582dba25a3c0175d8cc59b5502
jxq <jiangxq18@outlook.com>
2018-07-14 12:40:54 +0800

    Subsets

---
+++ Java/subsets.java
@@ -0,0 +1,21 @@
+//  Subsets
+
+// Time O(2^N), Space O(N)
+class Solution {
+    private List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    private List<Integer> subset = new ArrayList<Integer>();
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(nums, 0);
+        return this.ans;
+    }
+    
+    private void dfs(int[] nums, int start) {
+        this.ans.add(this.subset);
+        for (int i=start; i<nums.length; i++) {
+            this.subset.add(nums[i]);
+            dfs(nums, i+1);
+            this.subset.remove(this.subset.size()-1);
+        }
+    }
+}
