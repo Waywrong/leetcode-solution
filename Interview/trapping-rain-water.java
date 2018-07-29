e15e75d910f02dc394ae1c009a415da2adaccc22
Goffic <38147358+jiangxq18@users.noreply.github.com>
2018-07-29 09:43:06 +0800

    Trapping Rain Water

---
+++ Interview/trapping-rain-water.java
@@ -0,0 +1,14 @@
+//  Trapping Rain Water
+
+// Time O(N), Space O(1)
+class Solution {
+    public int trap(int[] height) {
+        int l = 0, r = height.length-1, level = 0, res = 0;
+        while (l < r) {
+            int lower = height[height[l]<height[r] ? l ++ : r --];
+            level = Math.max(level, lower);
+            res += level - lower;
+        }
+        return res;
+    }
+}
