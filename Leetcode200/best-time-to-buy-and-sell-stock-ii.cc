840c7552aa5a1be3dbf32419790261744c3f10ba
William Jiang <WilliamJiang@gmail.com>
2018-08-03 07:08:36 +0800

    Best Time to Buy and Sell Stock II

---
+++ Leetcode200/best-time-to-buy-and-sell-stock-ii.cc
@@ -0,0 +1,13 @@
+//  Best Time to Buy and Sell Stock II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2)  return 0;
+        int max_profit = 0;
+        for (int i=1; i<prices.size(); i++)
+            if (prices[i] > prices[i-1])
+                max_profit += prices[i] - prices[i-1];
+        return max_profit;
+    }
+};
