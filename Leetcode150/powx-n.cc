725c2a8cafc2ab9cfc39445c85b702ffe59eb533
William Jiang <WilliamJiang@gmail.com>
2018-08-05 12:33:53 +0800

    Pow(x,n)

---
+++ Leetcode150/powx-n.cc
@@ -0,0 +1,20 @@
+//  Pow(x,n)
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+        if (n < 0) {
+            n = abs(n);
+            x = 1 / x;
+        }
+        double t = myPow(x, n/2);
+        double res;
+        if (n % 2)
+            res = t * t * x;
+        else
+            res = t * t;
+        return isinf(res) ? 0 : res;
+    }
+};
