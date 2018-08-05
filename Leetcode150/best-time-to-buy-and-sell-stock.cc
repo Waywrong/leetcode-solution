9e6545f83f1743f4fe6bbfad224a0618a175a6c4
William Jiang <WilliamJiang@gmail.com>
2018-08-05 12:39:28 +0800

    Leetcode150

--- Leetcode150/best-time-to-buy-and-sell-stock.cc
+++ Leetcode150/best-time-to-buy-and-sell-stock.cc
@@ -5,18 +5,10 @@ public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() < 2)  return 0;
         int max_profit = 0, min_price = prices[0];
-        int buy_day = 0, sell_day = -1, pre_buy_day = 0;
-        for (int i=1; i<prices.size(); i++) {
-            if (prices[i] - min_price > max_profit) {
-                max_profit = prices[i] - min_price;
-                sell_day = i;
-            }
-            if (prices[i] < min_price) {
-                pre_buy_day = i;
-                min_price = prices[i];
-            }
+        for (int i = 1; i < prices.size(); i ++) {
+            max_profit = max(max_profit, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
         }
-        cout << pre_buy_day << "\t" << sell_day << endl;
         return max_profit;
     }
 };
