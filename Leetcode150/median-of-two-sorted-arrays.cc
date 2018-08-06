f1efe88107a4f189d5db91f383eda0eea64310a2
Goffic <38147358+jiangxq18@users.noreply.github.com>
2018-08-07 06:52:28 +0800

    Leetcode150

--- Leetcode150/median-of-two-sorted-arrays.cc
+++ Leetcode150/median-of-two-sorted-arrays.cc
@@ -36,23 +36,27 @@ public:
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int total = nums1.size() + nums2.size();
-        if (total % 2 == 1) {
-            return findKth(nums1, 0, nums2, 0, total / 2 + 1);
-        } else {
-            return (findKth(nums1, 0, nums2, 0, total / 2) + findKth(nums1, 0, nums2, 0, total / 2 + 1)) / 2;
-        }
+        int n = nums1.size() + nums2.size();
+        if (n % 2 == 1)
+            return findKth(nums1, 0, nums2, 0, n/2+1);
+        else
+            return (findKth(nums1, 0, nums2, 0, n/2) + findKth(nums1, 0, nums2, 0, n/2+1)) / 2.0;
     }
-    double findKth(vector<int> &nums1, int i, vector<int> &nums2, int j, int k) {
-        if (nums1.size() - i > nums2.size() - j) return findKth(nums2, j, nums1, i, k);
-        if (nums1.size() == i) return nums2[j + k - 1];
-        if (k == 1) return min(nums1[i], nums2[j]);
-        int pa = min(i + k / 2, int(nums1.size())), pb = j + k - pa + i;
-        if (nums1[pa - 1] < nums2[pb - 1]) 
-            return findKth(nums1, pa, nums2, j, k - pa + i);
-        else if (nums1[pa - 1] > nums2[pb - 1]) 
-            return findKth(nums1, i, nums2, pb, k - pb + j);
-        else 
-            return nums1[pa - 1];
+    
+    int findKth(vector<int> &nums1, int p1, vector<int> &nums2, int p2, int k) {
+        if (nums1.size()-p1 > nums2.size()-p2)
+            return findKth(nums2, p2, nums1, p1, k);
+        if (nums1.size() == p1)
+            return nums2[p2+k-1];
+        if (k == 1)
+            return min(nums1[p1], nums2[p2]);
+        int pos1 = min(p1 + k/2, (int)nums1.size());
+        int pos2 = p2 + k - pos1 + p1;
+        if (nums1[pos1-1] < nums2[pos2-1])
+            return findKth(nums1, pos1, nums2, p2, k-pos1+p1);
+        else if (nums1[pos1-1] > nums2[pos2-1])
+            return findKth(nums1, p1, nums2, pos2, k-pos2+p2);
+        else
+            return nums1[pos1-1];
     }
 };
