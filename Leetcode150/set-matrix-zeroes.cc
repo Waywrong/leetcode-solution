a870e9fbdb7a79634b5e208e42e6cd520fc3e3e4
Goffic <38147358+jiangxq18@users.noreply.github.com>
2018-08-14 11:26:44 +0800

    Leetcode150

--- Leetcode150/set-matrix-zeroes.cc
+++ Leetcode150/set-matrix-zeroes.cc
@@ -1,2 +1,24 @@
 //  Set Matrix Zeroes
 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        if (matrix.size()==0 || matrix[0].size()==0)
+            return;
+        unordered_set<int> rows, cols;
+        for (int i = 0; i < matrix.size(); i ++) {
+            for (int j = 0; j < matrix[i].size(); j ++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
+        }
+        for (auto &r : rows) 
+            for (int i = 0; i < matrix[0].size(); i ++)
+                matrix[r][i] = 0;
+        for (auto &c : cols)
+            for (int i = 0; i < matrix.size(); i ++)
+                matrix[i][c] = 0;
+    }
+};
