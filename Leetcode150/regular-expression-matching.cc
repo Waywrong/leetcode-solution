9c07c65c2f33efeac7826192c041a8f0ae448864
Goffic <38147358+jiangxq18@users.noreply.github.com>
2018-08-22 16:42:49 +0800

    Leetcode150

--- Leetcode150/regular-expression-matching.cc
+++ Leetcode150/regular-expression-matching.cc
@@ -1,5 +1,6 @@
 //  Regular Expression Expression
 
+/* Solution 2 */
 class Solution {
 public:
     bool isMatch(string s, string p) {
@@ -15,5 +16,25 @@ public:
             s = s.substr(1);
         }
         return isMatch(s, p.substr(2));
+    }
+};
+
+/* Solution 2 */
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<bool>> dp(s.length()+1, vector<bool>(p.length()+1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i ++)
+            dp[0][i] = i>1 && p[i-1]=='*' && dp[0][i-2];
+        for (int i = 1; i <= s.length(); i ++) {
+            for (int j = 1; j <= p.length(); j ++) {
+                if (p[j-1] != '*')
+                    dp[i][j] = dp[i-1][j-1] && (s[i-1]==p[j-1] || p[j-1]=='.');
+                else
+                    dp[i][j] = (dp[i][j-2]) || ((s[i-1]==p[j-2] || p[j-2]=='.') && dp[i-1][j]);
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 };
