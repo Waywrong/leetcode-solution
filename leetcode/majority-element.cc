302526ea6070b83df14769b564e089062e216cc5
Xiao-Quan Jiang <jiangxq18@outlook.com>
2018-10-29 10:31:40 +0800

    leetcode solution

---
+++ leetcode/majority-element.cc
@@ -0,0 +1,16 @@
+//  Majority Element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = 0, count = 0;
+        for (auto &x : nums) {
+            if (x == major) ++ count;
+            else {
+                if (count > 0)  -- count;
+                else    { major = x; count = 1; }
+            }
+        }
+        return major;
+    }
+};
